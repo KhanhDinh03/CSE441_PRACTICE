f3482aa33d19043da101ec0010c3a44596fe1e0e
KhanhDinh03 <dgkhanh2003@gmail.com>
2024-09-13 02:14:50 +0700

    12/09/2024 - exercise 1, 2

--- Bai_2/app/src/main/java/com/example/bai_2/MainActivity.kt
+++ Bai_2/app/src/main/java/com/example/bai_2/MainActivity.kt
@@ -2,7 +2,6 @@ package com.example.bai_2
 
 import android.content.Intent
 import android.os.Bundle
-import android.view.View
 import android.widget.Button
 import android.widget.EditText
 import android.widget.Toast
@@ -10,13 +9,12 @@ import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
-import kotlin.properties.Delegates
 
 class MainActivity : AppCompatActivity() {
     private lateinit var btnKetQua : Button
-    private var soA by Delegates.notNull<Int>()
-    private var soB by Delegates.notNull<Int>()
-    private var ketQua by Delegates.notNull<Int>()
+    private var soA: Int = 0
+    private var soB: Int = 0
+    private var ketQua: Float = 0.0f
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -29,10 +27,10 @@ class MainActivity : AppCompatActivity() {
         }
 
         // Lấy giá trị từ EditText và chuyển đổi sang kiểu Int
-        btnKetQua = findViewById<Button>(R.id.btn_ket_qua)
+        btnKetQua = findViewById(R.id.btn_ket_qua)
 
         // Hiển thị kết quả sang txt_ket_qua trong ResultActivity
-        btnKetQua.setOnClickListener(View.OnClickListener {
+        btnKetQua.setOnClickListener {
             // try catch để bắt lỗi khi người dùng nhập vào không phải số
             soA = try {
                 findViewById<EditText>(R.id.txt_a).text.toString().toInt()
@@ -49,11 +47,18 @@ class MainActivity : AppCompatActivity() {
             }
 
             // Giải phương trình bậc nhất
-            ketQua = soA + soB
-
+            ketQua = if (soA == 0) {
+                if (soB == 0) {
+                    Float.POSITIVE_INFINITY
+                } else {
+                    Float.NEGATIVE_INFINITY
+                }
+            } else {
+                -soB.toFloat() / soA
+            }
             val intent = Intent(this, ResultActivity::class.java)
             intent.putExtra("KET_QUA", ketQua)
             startActivity(intent)
-        })
+        }
     }
 }
--- Bai_2/app/src/main/java/com/example/bai_2/ResultActivity.kt
+++ Bai_2/app/src/main/java/com/example/bai_2/ResultActivity.kt
@@ -1,18 +1,15 @@
 package com.example.bai_2
 
-import android.annotation.SuppressLint
 import android.os.Bundle
 import android.widget.Button
 import android.widget.TextView
-import android.widget.Toast
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
-import kotlin.properties.Delegates
 
 class ResultActivity : AppCompatActivity() {
-    private var ketQua by Delegates.notNull<Int>()
+    private var ketQua : Float = 0.0f
     private lateinit var btnBack : Button
 
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -25,12 +22,11 @@ class ResultActivity : AppCompatActivity() {
             insets
         }
 
-        ketQua = intent.getIntExtra("KET_QUA", 0)
-
+        ketQua = intent.getFloatExtra("KET_QUA", 0.0f)
         // Gán giá trị kết quả vào txt_ket_qua
         val txtKetQua = findViewById<TextView>(R.id.txt_ket_qua)
         txtKetQua.text = ketQua.toString()
-        btnBack = findViewById<Button>(R.id.btn_back)
+        btnBack = findViewById(R.id.btn_back)
         btnBack.setOnClickListener {
             finish()
         }
